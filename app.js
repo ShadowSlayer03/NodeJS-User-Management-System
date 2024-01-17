b5de1862b13cc30178065fd19f9aae2b4496147a
ShadowSlayer03 <sslayer441@gmail.com>
2024-01-17 12:30:03 +0530

    Added the main server file

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+require('dotenv').config()
+
+const port = 3000;
+const app = express();
+
+// Body Parser as middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Setting a static folder
+app.use(express.static("public"));
+
+// Setting a view-engine -- Express Handlebars
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+// Connection Pool
+var pool = mysql.createPool({
+    connectionLimit: 100,
+    port: 3307,
+    host     : process.env.DB_HOST,
+    user     : process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password : process.env.DB_PASS
+});
+
+const routes = require('./server/routes/user');
+app.use('/',routes);
+
+app.listen(port, () => {
+    console.log("App running successfully!");
+});
